7279853906214361aac2ec5797feb8e764e5b7cf
shankar7042 <shankar.kumar.4171@gmail.com>
2023-08-17 11:43:05 +0530

    dynamic home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import { cn } from "@/lib/utils";
 import { HomeIcon } from "lucide-react";
 import Link from "next/link";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const session = await getAuthSession();
 
